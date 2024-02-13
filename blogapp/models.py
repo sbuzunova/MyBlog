b6f3372f5abe5406975667fd7d8aef247f1c11f3
sbuzunova <sophia.buzunova@gmail.com>
2024-02-13 20:07:30 +0100

    templates

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    contend = models.TextField()
+    published_date = models.DateTimeField(default = timezone.now)
+
+    def __str__(self):
+        return self.title
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from blogapp.models import Post
 
 # Create your views here.
+def post_list(request):
+    # post1 = Post(
+    #     title = "Python",
+    #     contend = "dfghuijopsdfghhjj",
+    # )
+    # post1.save()
+    posts = Post.objects.all()
+    context={
+        "posts": posts,
+    }
+    return render(request, 'blogapp/post_list.html', context=context)
